efeb87bad637641ad9bb250588c4de4bc2836962
pratikshelar546 <pratikshelar987@gmail.com>
2026-01-04 00:11:10 +0530

    implemented singlton for db connection

--- src/config/mongodbConnection.ts
+++ src/config/mongodbConnection.ts
@@ -1,13 +1,52 @@
-import { connect } from "mongoose";
+import mongoose, { connect, connection, Connection } from "mongoose";
 
-export const connectMongoDB = async () => {
-    try {
-        const { MONGO_CONNECTION_URL } = process.env;
-        console.log(MONGO_CONNECTION_URL);
-        await connect(MONGO_CONNECTION_URL || "");
-        console.log("connected to mongoDB");
-        return true;
-    } catch (error) {
-        console.log("Failed to Connect with MongoDB");
+class Database {
+    private static instance :Database;
+    private  connection: typeof mongoose | null = null;
+    private connectionPromise : Promise<typeof mongoose >| null = null;
+
+    private constructor(){}
+
+    public static getInstance():Database{
+        if(!Database.instance){
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    public async connect():Promise<typeof mongoose>{
+        if(this.connection && mongoose.connection.readyState ===1){
+            return this.connection
+        }
+
+        if(this.connectionPromise){
+            return this.connectionPromise
+            
+        }
+
+        const url = process.env.MONGO_CONNECTION_URL||"mongodb://localhost:27017/attendace-managment";
+
+        try{
+            this.connectionPromise = mongoose.connect(url,{
+                autoIndex:true
+            })
+            console.log("connected to mongodb");
+            this.connection = await this.connectionPromise;
+            return this.connection;
+            
+        }catch(error){
+            console.log("error while connecting to Database", error);
+            
+            throw error
+        }
+    }
+
+    public async disconnect():Promise<void>{
+        await mongoose.disconnect();
+        this.connection =null;
+        console.log("Disconnected from database");
+        
     }
 }
+
+export const connectDb = ()=>Database.getInstance().connect();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { registerRoutes } from './routes/routes';
-import { connectMongoDB } from "./config/mongodbConnection";
 import dotenv from "dotenv";
+import { connectDb } from "./config/mongodbConnection";
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 registerRoutes(app);
-connectMongoDB();
+connectDb();
 const PORT = 3000;
 app.listen(PORT,()=>{
     console.info("server is running on ",PORT)
